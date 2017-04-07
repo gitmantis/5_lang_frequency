0763232ee74fb96bb6565fe1a1d0c4cb8e575822
gitmantis <kolesnikov.andrey@gmail.com>
2017-04-08 00:36:59 +0300

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Скрипт lang_frequency.py вычисляет количество вхождений слов в заданный текст и выводит
+сортированный по убыванию популярности список c количеством вхождений. 
+
+Скрипт запускается с одним параметром: путь до файла с текстом.
+
+Пример запуска скрипта.
+python lang_frequency.py text
+
+Word: в => Frequency: 6
+Word: и => Frequency: 5
+Word: на => Frequency: 3
+Word: скрипт => Frequency: 3
+Word: должен => Frequency: 2
+Word: такой => Frequency: 2
+Word: можно => Frequency: 2
+Word: с => Frequency: 2
+Word: не => Frequency: 2
+Word: вход => Frequency: 2
+
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,21 @@
-
+import sys
+import os.path
+import re
+from collections import Counter
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r', encoding='utf-8', errors='ignore') as filetext:
+        return filetext.read()
 
 
 def get_most_frequent_words(text):
-    pass
-
+    words = re.findall(r'\w+', text.lower())
+    return Counter(words).most_common(10)
 
 if __name__ == '__main__':
-    pass
+    fpath = sys.argv[1]
+
+    for word in sorted(get_most_frequent_words(load_data(fpath)), key=lambda wd: wd[1], reverse=True):
+        print("Word: %s => Frequency: %s" % (word[0], str(word[1])))
